8f1ce3c66fb22cd52b66d238758f9ac67f128dc9
zhangzhibang0309 <295854713@qq.com>
2021-10-31 16:07:25 +0800

    更新

--- src/com/catalina/BST/BinarySearchTree.java
+++ src/com/catalina/BST/BinarySearchTree.java
@@ -1,8 +1,10 @@
 package com.catalina.BST;
 
+import com.catalina.BST.printer.BinaryTreeInfo;
+
 import java.util.Comparator;
 
-public class BinarySearchTree<E> {
+public class BinarySearchTree<E> implements BinaryTreeInfo {
     private int size;
     private Node<E> root;
     private Comparator<E> comparator;
@@ -45,6 +47,7 @@ public class BinarySearchTree<E> {
         while (node != null) {
             parent = node;
             cmp = compare(element, node.element);
+            System.out.println(cmp);
             if (cmp > 0) {
                 node = node.right;
             } else if (cmp < 0) {
@@ -82,7 +85,27 @@ public class BinarySearchTree<E> {
     public int compare(E e1, E e2) {
         if (comparator != null) return this.comparator.compare(e1, e2);
 
-        return ((Comparable<E>)e1).compareTo(e2);
+        return ((Comparable<E>) e1).compareTo(e2);
+    }
+
+    @Override
+    public Object root() {
+        return root;
+    }
+
+    @Override
+    public Object left(Object node) {
+        return ((Node<E>) node).left;
+    }
+
+    @Override
+    public Object right(Object node) {
+        return ((Node<E>) node).right;
+    }
+
+    @Override
+    public Object string(Object node) {
+        return ((Node<E>) node).element;
     }
 
     private static class Node<E> {
--- src/com/catalina/BST/Main.java
+++ src/com/catalina/BST/Main.java
@@ -1,5 +1,7 @@
 package com.catalina.BST;
 
+import com.catalina.BST.printer.BinaryTrees;
+
 import java.util.Comparator;
 
 public class Main {
@@ -20,6 +22,26 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        Integer data[] = new Integer[] {
+                7,4,9,2,5,8,11,3
+        };
+
+        BinarySearchTree<Integer> bst0 = new BinarySearchTree<>();
+        for (int i = 0; i < data.length; i++) {
+            bst0.add(data[i]);
+        }
+        BinaryTrees.print(bst0);
+
         BinarySearchTree<Person> bst = new BinarySearchTree<>(new PersonComparator());
+
+        BinarySearchTree<Person> bst2 = new BinarySearchTree<>(new PersonComparator2());
+        BinarySearchTree<Person> bst3 = new BinarySearchTree<>(new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getAge() - o2.getAge();
+            }
+        });
+
+        BinarySearchTree<Person> bst4 = new BinarySearchTree<>();
     }
 }
